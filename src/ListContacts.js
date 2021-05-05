a05658962840610b68b1a8b34f0553e8e5dc2a9c
untari <sastraembun0306@gmail.com>
2021-05-06 00:17:49 +0700

    create order list using props

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class ListContacs extends Component{
     render() {
-        console.log('Props, this.props');
+        
         return(
             <ol className='contact-list'>
-                
+                {this.props.contacts.map((contact) => (
+                    <li key={contact.id}>
+                        {contact.name}
+                    </li>
+                ))}
             </ol>
         )
     }
