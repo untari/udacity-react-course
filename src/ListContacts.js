b2e11bb61676f1a6aabd592e6136a8cb4f1cc605
untari <sastraembun0306@gmail.com>
2021-05-05 16:42:25 +0700

    create contact list

---
+++ src/ListContacts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListContacs extends Component{
+    render() {
+        console.log('Props, this.props');
+        return(
+            <ol className='contact-list'>
+                
+            </ol>
+        )
+    }
+}
+export default ListContacs;
