32cd4cd7d33d7644e4399bab70a10068dd21a971
untari <sastraembun0306@gmail.com>
2021-05-06 15:04:00 +0700

    adding filter based on the input field

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -16,9 +16,16 @@ class  ListContacts extends Component {
         }));
     };
     render() {
+        const { query } = this.state
+        const { contacts, onDeleteContact } = this.props
+        
+        const showingContacts = query === ''
+        ? contacts : contacts.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
+        
         return (
             <div className="list-contacts">
-                {JSON.stringify(this.state)}
                 <div  className="list-contacts-top">
                     <input 
                         className='search-contacts'
@@ -29,7 +36,7 @@ class  ListContacts extends Component {
                     />
                 </div>
                 <ol className="contact-list">
-                    {this.props.contacts.map(contact => (
+                    {showingContacts.map(contact => (
                         <li key={contact.id} className="contact-list-item">
                         <div
                             className="contact-avatar"
@@ -41,7 +48,7 @@ class  ListContacts extends Component {
                             <p>{contact.name}</p>
                             <p>{contact.handle}</p>
                         </div>
-                        <button onClick={() => this.props.onDeleteContact(contact)}
+                        <button onClick={() => onDeleteContact(contact)}
                         className="contact-remove">Remove</button>
                         </li>
                     ))}
