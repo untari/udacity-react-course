b15739ac7c06cb7b3f52c41fd8e8d1136e6e03de
untari <sastraembun0306@gmail.com>
2021-05-06 23:57:22 +0700

    adding link

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class  ListContacts extends Component {
     static propTypes = {
@@ -22,7 +23,7 @@ class  ListContacts extends Component {
     };
     render() {
         const { query } = this.state
-        const { contacts, onDeleteContact, onNavigate } = this.props
+        const { contacts, onDeleteContact } = this.props
         
         const showingContacts = query === ''
         ? contacts : contacts.filter((c) => (
@@ -39,12 +40,11 @@ class  ListContacts extends Component {
                         value={query}
                         onChange={this.updateQuery}
                     />
-                    <a
-                    href="#create"
-                    onClick={onNavigate}
+                    <Link
+                    to="/create"
                     className="add-contact"> 
                         Add Contact
-                    </a>
+                    </Link>
                 </div>
                 
                 {showingContacts.length !== contacts.length && (
